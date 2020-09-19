3182262c53f6ccd9afc6ec94c5de6ccee74a789b
pfreemanc <pfreemanc@gmail.com>
2020-09-19 17:55:39 -0400

    fixed shiny sprite issue, slight refactoring

--- script.js
+++ script.js
@@ -4,14 +4,10 @@ const pokedexApp = {};
 // creating .init
 pokedexApp.init = function () {
   const pokeNum = pokedexApp.genRandomNum();
-
   pokedexApp.pokemonChoice(pokeNum);
-
-  $(".btnRandom").on("click", function(){
-    pokedexApp.clearInfo();
+  $(".btnRandom").on("click", function () {
     pokedexApp.buttonRandom();
-  })
-  
+  });
 };
 
 // function using ID. ID param will be generated with genrandomnum function when called.
@@ -22,13 +18,12 @@ pokedexApp.pokemonChoice = function (id) {
     dataType: "json",
   }).then(function (result) {
     pokedexApp.displayInfo(result);
-    $("#spriteContainer").on('mouseenter', function() {
-      pokedexApp.shinySprites(result);
+    $("#spriteContainer").on("mouseenter", function () {
+      pokedexApp.showSprites(result, 'shiny');
     });
-    $('#spriteContainer').on('mouseleave', function(){
-      $('.sprites').empty();
-      pokedexApp.showSprites(result);
-    })
+    $("#spriteContainer").on("mouseleave", function () {
+      pokedexApp.showSprites(result, 'normal');
+    });
   });
 };
 
@@ -38,14 +33,16 @@ pokedexApp.displayInfo = function (poke) {
   // Name and ID
   $("#pokemonName").text(poke.name);
   $("#pokemonID").text(`#${poke.id}`);
+  // calling type function.
+  this.checkTypes(poke);
   // Ability
   const ability1 = poke.abilities[0].ability.name;
   $("#pokeAbility").text(ability1);
   // Height
-  const heightCM = (poke.height * 10)
+  const heightCM = poke.height * 10;
   $("#pokeHeight").text(`${heightCM} cm`);
   // Weight
-  const weightKG = (poke.weight * 0.1)
+  const weightKG = poke.weight * 0.1;
   $("#pokeWeight").text(`${weightKG} kg`);
   // Photo (main image)
   const mainImage = poke.sprites.other["official-artwork"].front_default;
@@ -57,43 +54,50 @@ pokedexApp.displayInfo = function (poke) {
   const move2 = poke.moves[1].move.name;
   $("#pokeMoves").append(`<li>${move2}</li>`);
   // Show sprites
-  pokedexApp.showSprites(poke);
+  this.showSprites(poke, 'normal');
 };
 
-// 
-pokedexApp.clearInfo = function(){
-  $(".textContainer ul").empty();
+// clears the information out of the UL
+pokedexApp.clearInfo = function () {
+  $("ul").empty();
+};
+
+// runs a for each loop, as there will be more than one object if pokemon has more than one type. adds to the ul with the type information.
+pokedexApp.checkTypes = function (pokeNum) {
+  pokeNum.types.forEach((element) => {
+    const listItem = $("<li>");
+    listItem.text(element.type.name);
+    $("#pokemonType").append(listItem);
+  });
+};
+
+// displays the sprites. has various valued stored, including the shiny sprites. parameters are the pokenunmber and the choice, where normal or shiny will be passed in. 
+pokedexApp.showSprites = function (poke, choice) {
   $('.sprites').empty();
-
-}
-
-// Function to display sprites
-pokedexApp.showSprites = function (poke) {
-  // 
-  const frontSprite = $('<img>');
-  $(frontSprite).attr('src', poke.sprites.front_default);
-  $('#frontSprite').append(frontSprite)
-  
-  // repeat the above for the backSprite
-  const backSprite = $('<img>');
-  $(backSprite).attr('src', poke.sprites.back_default);
-  $('#backSprite').append(backSprite);
+  const frontSprite = $("<img>");
+  const backSprite = $("<img>");
+  // storing the sprite values 
+  const frontDefault = poke.sprites.front_default;
+  const backDefault = poke.sprites.back_default;
+  const frontShiny = poke.sprites.front_shiny;
+  const backShiny = poke.sprites.back_shiny;
+  if(choice === 'normal') {
+    $(frontSprite).attr("src", frontDefault);
+    $(backSprite).attr("src", backDefault);
+  } else {
+    $(frontSprite).attr('src', frontShiny);
+    $(backSprite).attr('src', backShiny);
+  }
+  $("#frontSprite").append(frontSprite);
+  $("#backSprite").append(backSprite);
 };
-
-// Function to show shinies (activated on mouseenter)
-pokedexApp.shinySprites = function(poke) {
-  console.log('hi');
-  $('#frontSprite img').attr('src', poke.sprites.front_shiny);
-  $('#backSprite img').attr('src', poke.sprites.back_shiny)
-}
 
 // Button randomizer
-
-pokedexApp.buttonRandom = function(){
+pokedexApp.buttonRandom = function () {
+  this.clearInfo();
   const ranNum = pokedexApp.genRandomNum();
   pokedexApp.pokemonChoice(ranNum);
-}
-
+};
 
 // TODO random num function from 1-151
 pokedexApp.genRandomNum = function () {

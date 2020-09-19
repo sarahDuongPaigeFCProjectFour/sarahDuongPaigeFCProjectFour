b3cd1f9b7441c542840a6062400118914201b8d9
Sarah <sarahmichelleduong@gmail.com>
2020-09-19 12:41:36 -0400

    new additions

--- script.js
+++ script.js
@@ -12,44 +12,45 @@ pokedexApp.init = function() {
       method: "GET",
       dataType: "json",
     }).then(function (result) {
-
-      // testing if name appends
-      $("#pokemonName").text(result.name);
-      $('#pokemonID').text(result.id);
-
-      // Ability
-      const ability1 = result.abilities[0].ability.name;
-      $("#pokeAbility").text(ability1);
-
-      // Height
-      $("#pokeHeight").text(result.height);
-      
-      // Weight
-      $("#pokeWeight").text(result.weight);
-
-      // Moves (possibly randomize??)
-      // TO DO: FUNCTION
-
-      const move1 = result.moves[0].move.name;
-      $("#pokeMoves").append(`<li>${move1}</li>`)
-
-      const move2 = result.moves[1].move.name;
-      $("#pokeMoves").append(`<li>${move2}</li>`);
-
-      // Photo ** needs to be fixed
-
-      const mainImage = result.sprites.other['official-artwork'].front_default;
-
-      $("#pokemonMainImage").attr("src", mainImage);
-
+      displayInfo(result);
     });
   }
   pokemonChoice(genRandomNum())
+
 }
 
-// function that will append all text based info
+// Append function
 
-// TODO function that will append sprite for main image
+pokedexApp.displayInfo = function(poke){
+  // Name and ID
+  $("#pokemonName").text(poke.name);
+  $('#pokemonID').text(poke.id);
+  // Ability
+  const ability1 = poke.abilities[0].ability.name;
+  $("#pokeAbility").text(ability1);
+  // Height
+  $("#pokeHeight").text(poke.height);
+  // Weight
+  $("#pokeWeight").text(poke.weight);
+  // Photo (main image)
+  const mainImage = poke.sprites.other['official-artwork'].front_default;
+  $("#pokemonMainImage").attr("src", mainImage);
+  // Moves
+  const move1 = poke.moves[0].move.name;
+  $("#pokeMoves").append(`<li>${move1}</li>`)
+  const move2 = poke.moves[1].move.name;
+  $("#pokeMoves").append(`<li>${move2}</li>`);
+  // Show sprites
+  pokemonApp.showSprites(poke);
+}
+
+// Function to display sprites
+
+pokemonApp.showSprites = function(poke){
+  $(".frontSprite").html("hi")
+}
+
+
 
 
 // TODO random num function from 1-151
